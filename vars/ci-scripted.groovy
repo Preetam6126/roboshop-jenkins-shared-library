4a758c4fda01bf0c01e155350eb28d97f2d45d44
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-28 01:41:52 +0000

    fri28

---
+++ vars/ci-scripted.groovy
@@ -0,0 +1,26 @@
+def call() {
+ if (!env.sonar_extra_opts) {
+     env.sonar_extra_opts=""
+ }
+ 
+ node('workstation'){
+  
+  try { 
+   stage('Compile/Build') {
+    common.compile()
+   }
+   
+   stage('Test Cases') { 
+    common.testcases()
+   }
+ stage('Code Quality') {
+  
+  common.codequality()
+     }
+  } catch (e) {
+   
+   mail bcc: '', body: "critical look into ${component} \n ${BUILD_URL}", cc: '', from: 'preetamknowledge@gmail.com', replyTo: '', subject: "${component} - Pipeline Failed", to: 'preetamknowledge@gmail.com'
+   
+    }
+  }
+}
