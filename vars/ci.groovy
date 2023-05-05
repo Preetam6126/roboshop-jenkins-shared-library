16c8cc85ef18e82d0947044918825745aa176bd5
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-05-05 09:28:23 +0000

    again

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,46 +1,70 @@
-def call() {
- if (!env.sonar_extra_opts) {
-     env.sonar_extra_opts=""
- }
-  pipeline {
-  agent any
+// def call() {
+//  if (!env.sonar_extra_opts) {
+//      env.sonar_extra_opts=""
+//  }
+//   pipeline {
+//   agent any
   
-  stages {
+//   stages {
   
-    stage('Compile/Build') {
+//     stage('Compile/Build') {
     
-    steps {
+//     steps {
    
-     script {
+//      script {
     
-       common.compile()
+//        common.compile()
                    
-        }
+//         }
    
-      }
-    }
-   stage('Test Cases') { 
-    steps {
-     script {
-       common.testcases()
+//       }
+//     }
+//    stage('Test Cases') { 
+//     steps {
+//      script {
+//        common.testcases()
                    
-        }
-      }
-    }
-  stage('Code Quality') { 
-    steps {
-     script {
-       common.codequality()
+//         }
+//       }
+//     }
+//   stage('Code Quality') { 
+//     steps {
+//      script {
+//        common.codequality()
                    
-        }
-      }
-    }
-   }
+//         }
+//       }
+//     }
+//    }
    
-   post {
-     failure{
-        mail bcc: '', body: "critical look into ${component} \n ${BUILD_URL}", cc: '', from: 'preetamknowledge@gmail.com', replyTo: '', subject: "${component} - Pipeline Failed", to: 'preetamknowledge@gmail.com'
+//    post {
+//      failure{
+//         mail bcc: '', body: "critical look into ${component} \n ${BUILD_URL}", cc: '', from: 'preetamknowledge@gmail.com', replyTo: '', subject: "${component} - Pipeline Failed", to: 'preetamknowledge@gmail.com'
+//      }
+//    }
+//   }
+// }
+
+
+
+def call(){
+   pipeline {
+     agent any
+     
+     stages {
+      
+       stage('Compile/Build'){
+        steps {
+        echo 'Compile/build'
+        }
+       } 
+       
+       stage('Test cases'){
+        steps{
+        echo 'Test cases'
+        }
+       }
+       
      }
    }
-  }
 }
