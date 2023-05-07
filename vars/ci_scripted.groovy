98bdbb546c803f4e3b7e7185d2f83e764aa74f48
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-05-07 04:11:59 +0000

    ystoall

--- vars/ci_scripted.groovy
+++ vars/ci_scripted.groovy
@@ -7,11 +7,23 @@ def call() {
     
     try { 
    
+   stage('Check OUt Code') {
+    sh 'ls -l'
+     cleanWs()
+     sh 'ls -l'
+     git branch: 'main', url: 'https://github.com/Preetam6126/cart'
+     sh 'ls -l'
+   }
+   
+   sh 'env'
+   
+   if(env.BRANCH_NAME !=main) {
+   
     stage('Compile/Build') {
      sh 'env'
     common.compile()
      }
-  
+   }
      stage('Test Cases') { 
     common.testcases()
     }
