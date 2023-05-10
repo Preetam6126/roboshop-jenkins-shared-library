4f48182898fe96816bb541675f5995c71a429d40
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-05-10 00:07:46 +0000

    infra

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -25,7 +25,7 @@ def call() {
         
         stage('Apply') {
           steps {
-            sh 'terraform ${ACTION} -auto-approve -var-file=env-dev/main.tfvars' 
+            sh 'terraform ${ACTION} -auto-approve -var-file=env-${ENV}/main.tfvars' 
             }
           }
         }
