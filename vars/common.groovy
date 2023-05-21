5bf7b2bd7d1a4a044ca64b985c1b98c02abfcc8f
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-05-21 06:06:24 +0000

    yes

--- vars/common.groovy
+++ vars/common.groovy
@@ -68,7 +68,7 @@ def artifactupload() {
 // // }
 
 sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 598125288280.dkr.ecr.us-east-1.amazonaws.com'
-sh 'docker push 598125288280.dkr.ecr.us-east-1.amazonaws.com/${frontend}:${TAG_NAME}'
+sh 'docker push 598125288280.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
 }
 
 
